674ce466948328ffc917d5eee1cf147142b0a38e
Linkels <48438463+Linkels@users.noreply.github.com>
2019-03-27 11:14:36 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="css/style.css"/>
+
+        <title>CORPS</title>
+
+    </head>
+
+    <body>
+
+      <p class="image">
+        <img style="display:block; position:absolute; left:937px; top:177px;z-index:1" src="img/corps.png"alt="Mon image 1">
+
+
+        <img style="display:block; position:absolute; left:1000px; top:580px; width:37px; height:32px; z-index:1" src="img/Rond.png" width="137" height="132" alt="Mon image 1">
+          <span style="display:block; position:absolute; left:1200px; top:200px; width:37px; height:32px;">Partie supérieure du bras à l'endroit où il s'attache au tronc.</span>
+
+          <img style="display:block; position:absolute; left:960px; top:500px; width:37px; height:32px; z-index:1" src="img/Rond.png" width="137" height="132" alt="Mon image 1">
+            <span style="display:block; position:absolute; left:1200px; top:200px; width:37px; height:32px;">Partie du corps humain où la jambe s'articule avec la cuisse.</span>
+
+            <img style="display:block; position:absolute; left:935px; top:400px; width:37px; height:32px; z-index:1" src="img/Rond.png" width="137" height="132" alt="Mon image 1">
+              <span style="display:block; position:absolute; left:1200px; top:200px; width:37px; height:32px;">Partie du corps humain, servant à toucher et à prendre, située à l'extrémité du bras et munie de cinq doigts.</span>
+
+              <img style="display:block; position:absolute; left:940px; top:260px; width:37px; height:32px; z-index:1" src="img/Rond.png" width="137" height="132" alt="Mon image 1">
+               <span style="display:block; position:absolute; left:1200px; top:200px; width:37px; height:32px;">Tige rigide dont on se sert pour boucher un trou, assembler des pièces.</span>
+
+               <img style="display:block; position:absolute; left:1035px; top:350px; width:37px; height:32px; z-index:1" src="img/Rond.png" width="137" height="132" alt="Mon image 1">
+                <span style="display:block; position:absolute; left:1200px; top:200px; width:37px; height:32px;">Partie extérieure du bras, à l'endroit où il se plie.</span>
+
+                <img style="display:block; position:absolute; left:1010px; top:400px; width:37px; height:32px; z-index:1" src="img/Rond.png" width="137" height="132" alt="Mon image 1">
+                 <span style="display:block; position:absolute; left:1200px; top:200px; width:37px; height:32px;">Chacune des deux régions symétriques du corps formant saillie au-dessous de la taille.</span>
+  </p>
+
+
+    </body>
+
+     </html>
